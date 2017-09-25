a918191a73d902c713d8ec1c47c5fc124cad9cc9
out-sider <572558745@qq.com>
2017-09-25 13:09:39 +0800

    单片机程序
    tested on Arduino Uno 引脚需要根据实际连线修改

---
+++ callmeback.ino
@@ -0,0 +1,45 @@
+#define STEP 8
+#define EN 4
+#define M1 5
+#define M2 6
+#define M3 7
+char message = ' ';
+
+void move(){
+    digitalWrite(EN, LOW);delay(1);
+    
+    digitalWrite(STEP, LOW);delayMicroseconds(900);digitalWrite(STEP, HIGH);delayMicroseconds(700);
+    digitalWrite(STEP, LOW);delayMicroseconds(500);digitalWrite(STEP, HIGH);delayMicroseconds(500);
+    digitalWrite(STEP, LOW);delayMicroseconds(500);digitalWrite(STEP, HIGH);delayMicroseconds(500);
+    digitalWrite(STEP, LOW);delayMicroseconds(700);digitalWrite(STEP, HIGH);delayMicroseconds(900);
+
+    
+    Serial.println("ok");
+}
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(EN, OUTPUT);pinMode(M1, OUTPUT);pinMode(M2, OUTPUT);pinMode(M3, OUTPUT);pinMode(STEP, OUTPUT);
+  digitalWrite(M1, HIGH);digitalWrite(M2, HIGH);digitalWrite(M3, HIGH);digitalWrite(EN, HIGH);digitalWrite(STEP, HIGH);
+  
+}
+
+void loop() {
+  if(Serial.available())
+    message = Serial.read();
+    
+  if (message == 'r')
+    Serial.println("ready");    
+  else if (message == 'm')
+    move(); 
+  else if (message == 's')
+    digitalWrite(EN, HIGH);
+    
+  message = ' ';
+}
+
+
+
